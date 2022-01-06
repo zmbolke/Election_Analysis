9c76674a3ad9a6c7a384de34e8c22f8dce0517ab
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-01-06 16:52:31 -0500

    Nested boolean updated

--- Nested_Ifelse.py
+++ Nested_Ifelse.py
@@ -3,16 +3,12 @@ score = int(input('What is you test score?'))
 #If above 90 than A
 if score >= 90:
     print('Your grade is an A.')
-else: 
-    #If above 80 than B...
-    if score >= 80:
-        print('Your grade is a B.')
-    else:
-        if score >= 70:
-            print('Your grade is a C.')
-        else:
-            if score >= 60:
-                print('Your score is a D.')
-            else:
-                print('Your grade is an F.') 
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your score is a D.')
+else:
+    print('Your grade is an F.') 
                    
