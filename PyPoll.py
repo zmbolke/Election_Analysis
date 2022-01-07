2ff6c2f004b2e0d5bee8d33b0a754c8ae9e624df
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-01-07 09:22:13 -0500

    Headers

--- PyPoll.py
+++ PyPoll.py
@@ -1,21 +1,22 @@
 #PyPoll Project
 #1 Find the total number fo votes cast 
+
+#Add dependancies
 import csv
 import os
-from posixpath import join
-#Assign a variable to the file
+#Assign a variable to the load file
 file_to_load = os.path.join("Resources", "election_results.csv")
+#Assign a variable to the save file
+file_to_save = os.path.join("Analysis", "election_analysis.txt")
 #Open elction results and read file
 with open(file_to_load) as election_data:
-    print(election_data)
-#Create a file to save
-file_to_save = os.path.join("Analysis", "Election_Analysis.txt")
-#Open file_to_save as a text file
-with open(file_to_save, "w") as txt_file:
-    #List Counties
-    txt_file.write("Arapahoe\nDenver\nJefferson")
-
+    #read file and print data
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+    
 #Perform analysis
+
 
 #2 Get a complete list of candidates who received votes
 #3 Number of votes poer candidate
