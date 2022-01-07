b52d5d4ad00a6245b29073f75ef6436434762291
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-01-07 06:37:50 -0500

    Adding loops to declare variables

--- Python_Practice.py
+++ Python_Practice.py
@@ -1,3 +1,10 @@
 counties = ['Arapahoe', 'Denver', 'Jefferson']
 if counties[1]== 'Denver':
     print(counties[1])
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not in the list of counties.")
+for county in counties:
+    print(county)
+        
