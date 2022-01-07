fa65583d6617108edcb9608764d9c009c1e6ebb6
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-01-07 07:32:03 -0500

    Roadmap

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+#PyPoll Project
+#1 Find the total number fo votes cast 
+#2 Get a complete list of candidates who received votes
+#3 Number of votes poer candidate
+#4 Percentage of votes per candidate
+#5 The winner of the election based on popular vote
