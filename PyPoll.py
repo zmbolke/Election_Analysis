5b0412da56b821ba8f7f70348844564e1c7dd128
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-01-07 09:31:46 -0500

    Adding vote count

--- PyPoll.py
+++ PyPoll.py
@@ -1,20 +1,31 @@
 #PyPoll Project
 #1 Find the total number fo votes cast 
-
 #Add dependancies
 import csv
 import os
+
 #Assign a variable to the load file
 file_to_load = os.path.join("Resources", "election_results.csv")
 #Assign a variable to the save file
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
+
+#set total vote equal to 0
+total_votes = 0
+
 #Open elction results and read file
 with open(file_to_load) as election_data:
     #read file and print data
     file_reader = csv.reader(election_data)
     headers = next(file_reader)
-    print(headers)
-    
+    #Loop through rows
+    for row in file_reader:
+        #add up votes
+        total_votes += 1
+
+#Print total votes
+print(total_votes)
+
+
 #Perform analysis
 
 
